4ab354d09913e3a65477f486826ba1005776941b
yifan2000 <zhang488389@gmail.com>
2018-09-12 18:47:34 +0800

    第一次提交

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace p_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int n1, n2;
+            System.Console.Write("please input an int a: ");
+            a = Console.ReadLine();
+            n1 = Int32.Parse(a);
+            string b;
+            System.Console.Write("please input another int b: ");
+            b = Console.ReadLine();
+            n2 = Int32.Parse(b);
+            Console.WriteLine("The product of a*b: ");
+            Console.WriteLine(n1 * n2);
+
+        }
+    }
+}
---
+++ Homework1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static int txtConvertToInt(string txt)//转换文本
+        {
+            int result = 0;
+            bool f = int.TryParse(txt, out result);
+            return result;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = txtConvertToInt(textBox1.Text);
+            int num2 = txtConvertToInt(textBox2.Text);
+            int num = num1 * num2;
+            string numm = num.ToString();
+            MessageBox.Show("The product is: " + numm);
+        }
+    }
+}
