f9724cb8f983d1d5513f8c5d2a148f20747366a6
yifan2000 <zhang488389@gmail.com>
2018-10-26 00:04:55 +0800

    .

--- HomeworkWeek6/GreatOrderTests/OrderServiceTests.cs
+++ HomeworkWeek6/GreatOrderTests/OrderServiceTests.cs
@@ -9,13 +9,49 @@ using System.Xml.Serialization;
 using static GreatOrder.OrderDetails;
 
 
-
+        
 
 namespace GreatOrder.Tests
 {
     [TestClass()]
     public class OrderServiceTests
     {
+       
+
+        [TestMethod()]
+        public void CheckOrderIDTest()
+        {
+            OrderDetails[] orderDetail1 = { new OrderDetails("A", 5, 20)
+                , new OrderDetails("B", 6, 6), new OrderDetails("C", 7, 8) };
+            Order order1 = new Order(1, "aaa", orderDetail1);
+            OrderDetails[] orderDetail2 = { new OrderDetails("A", 5, 7)
+                , new OrderDetails("B", 6, 5), new OrderDetails("C", 7, 6) };
+            Order order2 = new Order(2, "aab", orderDetail2);
+            OrderService orderservice1 = new OrderService();
+            OrderService orderservice2 = new OrderService();
+            bool exist = orderservice1.CheckOrderID(1);
+            Assert.AreEqual(exist, true);
+        }
+
+        [TestMethod()]
+        public void DeleteOrderTest()
+        {
+            OrderDetails[] orderDetail1 = { new OrderDetails("A", 5, 20)
+                , new OrderDetails("B", 6, 6), new OrderDetails("C", 7, 8) };
+            Order order1 = new Order(1, "aaa", orderDetail1);
+            OrderDetails[] orderDetail2 = { new OrderDetails("A", 5, 7)
+                , new OrderDetails("B", 6, 5), new OrderDetails("C", 7, 6) };
+            Order order2 = new Order(2, "aab", orderDetail2);
+            OrderService orderservice1 = new OrderService();
+            OrderService orderservice2 = new OrderService();
+            orderservice1.orders.Add(order1);
+            orderservice1.orders.Add(order2);
+            orderservice1.orders.Add(order1);
+            orderservice1.orders.Add(order2);
+            orderservice2.orders.Add(order2);
+            orderservice1.DeleteOrder(1);
+            Assert.AreEqual(orderservice1.orders[0].NextCilentName, orderservice2.orders[0].NextCilentName);
+        }
         [TestMethod()]
         public void XmlSerializerExportTest()
         {
@@ -33,6 +69,7 @@ namespace GreatOrder.Tests
             Assert.AreEqual(success, true);
         }
 
+        
         [TestMethod()]
 
         //反序列化（正确输入）
